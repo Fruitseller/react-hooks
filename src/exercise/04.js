b0e7e23f5309f94aac1841acb2ea169f292452e2
Piotr Großmann <grossmann@sipgate.de>
2020-12-08 16:00:38 +0100

    Finished "useState: tic tac toe"

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,45 +2,13 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
-function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
-
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-  }
-
-  function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
-  }
+function Board({squares, onClick}) {
 
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button className='square' onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     )
@@ -48,38 +16,62 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
-      <div className="board-row">
+      <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
-      <div className="board-row">
+      <div className='board-row'>
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
-      <div className="board-row">
+      <div className='board-row'>
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 function Game() {
+  const [allSquares, setAllSquares] = React.useState([Array(9).fill(null)])
+  const [moves, setMoves] = React.useState(0)
+  const currentSquares = allSquares[moves]
+  const nextValue = calculateNextValue(currentSquares)
+  const winner = calculateWinner(currentSquares)
+  const status = calculateStatus(winner, currentSquares, nextValue)
+
+  function restart() {
+    setAllSquares(Array(1).fill(Array(9).fill(null)))
+  }
+
+  function selectSquare(square) {
+    if (winner || currentSquares[square]) {
+      return
+    }
+    const newAllSquares = [...allSquares]
+    newAllSquares[moves][square] = nextValue
+    setMoves(moves + 1)
+    setAllSquares(newAllSquares)
+  }
+
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
+    <div className='game'>
+      <div className='game-board'>
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className='restart' onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className='game-info'>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
+
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -87,8 +79,8 @@ function calculateStatus(winner, squares, nextValue) {
   return winner
     ? `Winner: ${winner}`
     : squares.every(Boolean)
-    ? `Scratch: Cat's game`
-    : `Next player: ${nextValue}`
+      ? `Scratch: Cat's game`
+      : `Next player: ${nextValue}`
 }
 
 // eslint-disable-next-line no-unused-vars
